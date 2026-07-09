f4982ada5a538db75cb170c4a85165cd8b0a33be
Егор Парублев <prakaznikor@gmail.com>
2026-07-09 18:23:07 +0300

    Задание 7

--- CyberSportsPortal.Core/Services/AdvertisementTasksService.cs
+++ CyberSportsPortal.Core/Services/AdvertisementTasksService.cs
@@ -5,12 +5,46 @@ namespace CyberSportsPortal.Core.Services;
 
 public class AdvertisementTasksService
 {
+    // Задание 7
     public List<KeyValuePair<int, int>> GetProbabilities(List<AdvertisingCompanyView> companies)
     {
-        return new List<KeyValuePair<int, int>>
+        List<KeyValuePair<int, int>> result = new List<KeyValuePair<int, int>>();
+
+        int currentYear = System.DateTime.Now.Year;
+        decimal totalPayments = 0;
+        Dictionary<int, decimal> companyTotals = new Dictionary<int, decimal>();
+
+        foreach (var company in companies)
         {
-            new KeyValuePair<int, int>(1,50),
-            new KeyValuePair<int, int>(2,50)
-        };
+            decimal companyTotal = 0;
+
+            foreach (var payment in company.PaymentInfo)
+            {
+                if (payment.PaymentDate.Year == currentYear)
+                {
+                    companyTotal += payment.PaymentSum;
+                }
+            }
+
+            companyTotals[company.Id] = companyTotal;
+            totalPayments += companyTotal;
+        }
+
+        if (totalPayments == 0)
+        {
+            return result;
+        }
+
+        foreach (var company in companies)
+        {
+            int probability = (int)(companyTotals[company.Id] / totalPayments * 100);
+
+            if (probability < 1)
+            {
+                probability = 1;
+            }
+            result.Add(new KeyValuePair<int, int>(company.Id, probability));
+        }
+        return result;
     }
 }
